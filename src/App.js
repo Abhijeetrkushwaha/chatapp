60d17f96ec347c9a5f9499cf5807490088a4cde5
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-08-20 19:17:42 +0530

    added bye

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 // import './App.css';
-
 function App() {
   return (
     <div className="App">
+      <h1>Hi  bye bye</h1>
     </div>
   );
 }
-
-export default App;
+export default App
