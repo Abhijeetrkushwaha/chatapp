f3d812da53ce7491ea2704488abc0fb4019de2b3
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-08-21 20:35:30 +0530

    added message component

--- src/components/AddText.js
+++ src/components/AddText.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Messages from './Messages'
 
 const AddText = () => {
+    
+    const [input, setInput] = useState('')
+    const [messages, setMessages] = useState([]);
+
+    const sendMessage = (e) => {
+
+        e.preventDefault()
+        setMessages([...messages, input])
+        setInput('')
+
+    }
+
+    const text = messages ? (
+        messages.map((message) => {
+            return <Messages message={message} key={Math.random()} />
+        })
+    ) : null ;
+
     return (
         <div className="container">
-            <form>
-                <input type="text"/>
-                <button>Send</button>
+            <form onSubmit={sendMessage}>
+                <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+                <button>Send message</button>
             </form>
+
+            {text}
+
         </div>
     )
 }
---
+++ src/components/Messages.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Messages({ message}) {
+    
+
+    return (
+        <div>
+           <p>message: {message} </p>
+        </div>
+    )
+}
+
+export default Messages
