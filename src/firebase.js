44137813eca3366c23eb2c5e136f6931c3ec1aed
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-08-25 06:20:28 +0530

    set env

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ const firebaseApp = firebase.initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: "chatapp-a778c",
+    projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDING_ID,
     appId: process.env.REACT_APP_API_ID,
